bd52159e431a068b91f8008f12f145248b2f4339
Tim Groeneboom <tim@timgroeneboom.com>
2019-04-18 14:35:54 +0200

    worked on webserver

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,3 +1,5 @@
+var webserver = require('./webserver.js').go();
+
 var http = require('http');
 var spreadsheet = require('google-spreadsheet');
 var async = require('async');
@@ -5,6 +7,10 @@ var async = require('async');
 // spreadsheet key is the long id in the sheets URL
 var doc = new spreadsheet('1wF8Mqemvph9G4DmANDbDHapTUVBIWyL9EKNetHuucso');
 var sheet;
+
+var log = {
+  devices : []
+};
 
 var setAuth = function setAuth(step) {
   // see notes below for authentication instructions!
@@ -33,7 +39,20 @@ const server = http.createServer(function(request, response) {
         var json = JSON.parse(data);
         if( json ){
           console.log(json);
+
+          // add it to the log
+          var deviceLog = log.devices[json.dev_id];
+          if( deviceLog == undefined )
+          {
+            deviceLog = {
+              decibels : []
+            };
+            log.devices[json.dev_id];
+          }
+          deviceLog.decibels.push(json.payload_fields.decibel);
+          webserver.addDecibel(json.dev_id, json);
   
+          //
           async.series([
             setAuth,
             function getInfoAndWorksheets(step) {
@@ -41,7 +60,8 @@ const server = http.createServer(function(request, response) {
                 console.log('Loaded doc: '+info.title+' by '+info.author.email);
                 sheet = info.worksheets[0];
                 console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
-  
+
+                // add it to google sheet
                 doc.addRow(1, {
                   time : json.metadata.time,
                   device_id : json.dev_id,
@@ -82,3 +102,5 @@ const server = http.createServer(function(request, response) {
 const port = 8080
 server.listen(port)
 console.log(`Listening at http://${port}`)
+
+
