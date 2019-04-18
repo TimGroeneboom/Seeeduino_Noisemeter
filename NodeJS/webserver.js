47f06a91154ec691c7bb87aac63b40d35ea02127
Tim Groeneboom <tim@timgroeneboom.com>
2019-04-18 17:59:54 +0200

    no message

--- NodeJS/webserver.js
+++ NodeJS/webserver.js
@@ -1,5 +1,5 @@
 
-exports.go = function (){
+exports.app = function (){
     const express = require('express');
     const app = express();
     const server = require('http').Server(app);
@@ -12,9 +12,6 @@ exports.go = function (){
     // improved database
     const dbFile = Path.join(__dirname, 'db.json');
 
-    //
-    var functions = {};
-    
     // Some options for express (node.js web app library)
     hbs.registerPartials(__dirname + '/views/partials');
     app.use(express.static(__dirname + '/public'));
@@ -85,59 +82,8 @@ exports.go = function (){
         });
     });
 
-    //
-    functions.addDecibel = function (devId, payload) {
-        console.log('[%s] Received uplink', appId, devId, payload.payload_fields);
-
-        appId = 'noiselogger';
-        let key = appId + ':' + devId;
-        let d = devices[key] = devices[key] || {};
-        d.eui = payload.hardware_serial;
-
-        for (let mapKey of Object.keys(dataMapping)) {
-            d[mapKey] = d[mapKey] || [];
-        }
-         d.lat = payload.payload_fields.lon;
-            d.lng = payload.payload_fields.lat;
-
-
-        for (let mapKey of Object.keys(dataMapping)) {
-            let v;
-            try {
-                v = dataMapping[mapKey].data(payload);
-            }
-            catch (ex) {
-                console.error('dataMapping[' + mapKey + '].data() threw an error', ex);
-                throw ex;
-            }
-            console.log(v, typeof v);
-
-            if (typeof v !== 'undefined') {
-                d[mapKey].push({
-                    ts: new Date(payload.metadata.time),
-                    value: v
-                });
-
-                io.emit('value-change', mapKey, {
-                    appId: appId,
-                    devId: devId,
-                    eui: d.eui,
-                    lat: d.lat,
-                    lng: d.lng
-                }, payload.metadata.time, v);
-            }
-        }
-    }
-    
+      
     io.on('connection', socket => {
-        /*
-        socket.on('connect-application', (appId, accessKey) => {
-            console.log('Connecting to application', appId, accessKey);
-            connectApplication(appId, accessKey)
-                .then(() => socket.emit('connected', appId))
-                .catch(err => socket.emit('connect-failed', JSON.stringify(err)));
-        });*/
-    
         socket.on('location-change', (appId, devId, lat, lng) => {
             let key = appId + ':' + devId;
             if (!devices[key]) {
@@ -165,100 +111,7 @@ exports.go = function (){
     server.listen(port, function () {
         console.log('Web server listening on port %s!', port);
     });
-    
-    function connectApplication(appId, accessKey) {
-        if (applications[appId]) {
-            if (!applications[appId].client) {
-                throw 'Already connecting to app ' + appId;
-            }
-            applications[appId].client.close();
-            delete applications[appId];
-        }
-    
-        applications[appId] = {
-            accessKey: accessKey
-        }
-    
-        console.log('[%s] Connecting to TTN', appId);
-        return new Promise((resolve, reject) => {
-    
-            return ttn.DataClient(appId, accessKey, 'eu.thethings.network:1883').then(client => {
-                applications[appId].client = client;
-    
-                client.on('error', (err) => {
-                    if (err.message === 'Connection refused: Not authorized') {
-                        console.error('[%s] Key is not correct', appId);
-                        client.close();
-                        delete applications[appId];
-                    }
-                    reject(err);
-                });
-    
-                client.on('connect', () => {
-                    console.log('[%s] Connected over MQTT', appId);
-                    resolve();
-                });
-    
-                client.on('uplink', (devId, payload) => {
-                    // on device side we did /100, so *100 here to normalize
-                    if (typeof payload.payload_fields.analog_in_1 !== 'undefined') {
-                        payload.payload_fields.analog_in_1 *= 100;
-                    }
-    
-                    console.log('[%s] Received uplink', appId, devId, payload.payload_fields);
-    
-                    let key = appId + ':' + devId;
-                    let d = devices[key] = devices[key] || {};
-                    d.eui = payload.hardware_serial;
-    
-                    for (let mapKey of Object.keys(dataMapping)) {
-                        d[mapKey] = d[mapKey] || [];
-                    }
-    
-                    if (!d.lat) {
-                        d.lat = mapCenter.lat + (Math.random() / 10 - 0.05);
-                    }
-                    if (!d.lng) {
-                        d.lng = mapCenter.lng + (Math.random() / 10 - 0.05);
-                    }
-    
-                    for (let mapKey of Object.keys(dataMapping)) {
-                        let v;
-                        try {
-                            v = dataMapping[mapKey].data(payload);
-                        }
-                        catch (ex) {
-                            console.error('dataMapping[' + mapKey + '].data() threw an error', ex);
-                            throw ex;
-                        }
-                console.log(v, typeof v);
-    
-                        if (typeof v !== 'undefined') {
-                            d[mapKey].push({
-                                ts: new Date(payload.metadata.time),
-                                value: v
-                            });
-    
-                            io.emit('value-change', mapKey, {
-                                appId: appId,
-                                devId: devId,
-                                eui: d.eui,
-                                lat: d.lat,
-                                lng: d.lng
-                            }, payload.metadata.time, v);
-                        }
-                    }
-                });
-    
-                console.log('[%s] Acquired MQTT client, connecting...', appId);
-            }).catch(err => {
-                console.error('[%s] Could not connect to The Things Network', appId, err);
-                delete applications[appId];
-                reject(err);
-            });
-        });
-    }
-    
+
     function exitHandler(options, err) {
         if (err) {
             console.error('Application exiting...', err);
@@ -285,6 +138,51 @@ exports.go = function (){
     process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
     process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
     process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
+    return {
+        addPayload : addPayload = function (payload) {
+            var devId = payload.dev_id;
+            appId = 'noiselogger';
+            console.log('[%s] Received uplink', appId, devId, payload.payload_fields);
     
-    return functions;
+            
+            let key = appId + ':' + devId;
+            let d = devices[key] = devices[key] || {};
+            d.eui = payload.hardware_serial;
+    
+            for (let mapKey of Object.keys(dataMapping)) {
+                d[mapKey] = d[mapKey] || [];
+            }
+             d.lat = payload.payload_fields.lon;
+                d.lng = payload.payload_fields.lat;
+    
+    
+            for (let mapKey of Object.keys(dataMapping)) {
+                let v;
+                try {
+                    v = dataMapping[mapKey].data(payload);
+                }
+                catch (ex) {
+                    console.error('dataMapping[' + mapKey + '].data() threw an error', ex);
+                    throw ex;
+                }
+                console.log(v, typeof v);
+    
+                if (typeof v !== 'undefined') {
+                    d[mapKey].push({
+                        ts: new Date(payload.metadata.time),
+                        value: v
+                    });
+    
+                    io.emit('value-change', mapKey, {
+                        appId: appId,
+                        devId: devId,
+                        eui: d.eui,
+                        lat: d.lat,
+                        lng: d.lng
+                    }, payload.metadata.time, v);
+                }
+            }
+        }
+    }
 }
---
+++ nodejs/app.js
@@ -0,0 +1,39 @@
+
+var app = function(){
+    var webserver = require('./webserver.js').app();
+    var spreadsheet = require('./spreadsheet.js').app();
+    
+    var http = require('http');
+
+    const server = http.createServer(function(request, response) {
+        console.dir(request.param)
+
+        if (request.method == 'POST') {
+          console.log('POST')
+          var body = ''
+          request.on('data', function(data) {
+            body += data
+
+            try{
+                var json = JSON.parse(data);
+                webserver.addPayload(data);
+                spreadsheet.addPayload(data);
+            }catch(error){
+                console.log(error);
+            }
+          });
+          request.on('end', function() {
+            console.log('Body: ' + body)
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.end('post received')
+          })
+        } 
+      })
+      
+      const port = 8080
+      server.listen(port)
+      console.log(`Listening at http://${port}`);
+}
+app();
+
+  
